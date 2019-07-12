741e6d5eedc5ff2b947d27a421f0fcdfb62d340c
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-11 21:45:52 -0700

    rest api working

---
+++ src/main/java/neverEatAlone/UserException.java
@@ -0,0 +1,12 @@
+package neverEatAlone;
+
+public class UserException extends Exception {
+	 public UserException() {
+	        super();
+	    }
+
+	    public UserException(String message) {
+	        super(message);
+	    }
+
+}
