77e2d331621d8bed4fe8cce3531b48e1c49a16f2
Sophearychiv <sopheary.chiv@gmail.com>
2019-07-14 02:09:12 -0700

    able to read data from the database and render as json to the api

--- src/main/java/neverEatAlone/APIs.java
+++ src/main/java/neverEatAlone/APIs.java
@@ -10,13 +10,14 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.sql.*;
 
 import com.google.gson.Gson;
 
 public class APIs {
     public static void main(String[] args) throws SQLException {
     	
-
+    	
          
     	
         final UserService userService = new UserServiceMapImpl();
@@ -27,8 +28,6 @@ public class APIs {
             User user = new Gson().fromJson(request.body(), User.class);
             userService.addUser(user);
             
-            System.out.println("executed");
-            
         	String url = "jdbc:mysql://localhost:3306/users";
         	String databaseUser = "student";
         	String password = "student";
@@ -36,7 +35,6 @@ public class APIs {
         	 Connection myConn = null;
              Statement myStmt = null;
       
-//             try {
                  // 1. Get a connection to database
                  myConn = DriverManager.getConnection(url, databaseUser, password);
       
@@ -45,23 +43,12 @@ public class APIs {
       
                  // 3. Execute SQL query
                  String sql = "insert into users " + " (last_name, first_name, email)"
-                         + " values ('" + user.getLastName() + "','" + user.getFirstName() + "','" + user.getEmail() + "')";
+                		 + " values ('" + user.getLastName() + "','" + user.getFirstName() + "','" + user.getEmail() + "')";
 
                  myStmt.executeUpdate(sql);
       
-//                 System.out.println("Insert complete.");
-//             } catch (Exception exc) {
-//                 exc.printStackTrace();
-//             } finally {
-//                 if (myStmt != null) {
-//                     myStmt.close();
-//                 }
-//      
-//                 if (myConn != null) {
-//                     myConn.close();
-//                 }
-//             }
-            
+                 System.out.println("Insert complete.");
+
 
             return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS));
             
@@ -75,8 +62,60 @@ public class APIs {
 
         get("/users/:id", (request, response) -> {
             response.type("application/json");
+            
+            String url = "jdbc:mysql://localhost:3306/users";
+        	String databaseUser = "student";
+        	String password = "student";
+        	
+    		 Connection myConn = null;
+    		 PreparedStatement preparedStmt = null;
+     		ResultSet myRs = null;
+            
+            myConn = DriverManager.getConnection(url, databaseUser , password);
+			
+			// 2. Prepare statement
+			preparedStmt = myConn.prepareStatement("select * from users where id = ?");
+			
+			// 3. Set the parameters
+//			preparedStmt.setDouble(1, Integer.parseInt(request.params(":id")));
+			preparedStmt.setString(1, request.params(":id"));
+			
+			// 4. Execute SQL query
+			myRs = preparedStmt.executeQuery();
+			String user = null;
+			
+			while (myRs.next()) {
+				String id = myRs.getString("id");
+				String lastName = myRs.getString("last_name");
+				String firstName = myRs.getString("first_name");
+				String email = myRs.getString("email");
+				
+//				System.out.printf("%s, %s", lastName, firstName);
+				user = new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(new User(id, firstName, lastName, email))));
+			}
+			
+			return user;
+			
+			
+			
+//			String id = request.params(":id");
+//			String lastName = myRs.getString("last_name");
+//			String firstName = myRs.getString("first_name");
+//			String email = myRs.getString("email");
+			
+//			System.out.printf("%s, %s", lastName, firstName);
+//			
+//			System.out.println(firstName);
+			
+			// 5. Display the result set
+//			display(myRs);
+			
+//			user = new User("1", "Adelyn", "Cheng", "adelyn@ada.com");
+			
+//            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(new User(id, firstName, lastName, email))));
+			
 
-            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUser(request.params(":id")))));
+//            return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUser(request.params(":id")))));
         });
 
         put("/users/:id", (request, response) -> {
@@ -106,5 +145,14 @@ public class APIs {
         });
 
     }
+    
+    private static void display(ResultSet myRs) throws SQLException {
+		while (myRs.next()) {
+			String lastName = myRs.getString("last_name");
+			String firstName = myRs.getString("first_name");
+			
+			System.out.printf("%s, %s", lastName, firstName);
+		}
+	}
 
 }
